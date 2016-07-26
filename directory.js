2f99952079f0bb06a0de7c63ea553b79355a548b
tothpeter <tothpeter08@gmail.com>
2016-07-26 10:47:28 +0200

    6-form event

--- directory.js
+++ directory.js
@@ -4,4 +4,14 @@ angular.module('directoryApp', [])
       {name: 'Toma', age: 11},
       {name: 'Kalina.tech', age: 1}
     ];
+
+    $scope.addPerson = function() {
+      $scope.list.push({
+        name: $scope.name,
+        age: $scope.age
+      });
+
+      $scope.name = '';
+      $scope.age = '';
+    }
   });
